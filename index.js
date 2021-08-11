d6fa4c6bc93f27e0b5b699e3b244f05d7d1a2998
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-11 20:59:29 +0700

    Add page logic

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const img = document.querySelector("img");
+const nextGifBtn = document.getElementById("next-gif");
+const searchBox = document.getElementById("search");
+const searchBtn = document.getElementById("search-button");
+const errorDisplay = document.getElementById("error");
 
-fetch(
-	"https://api.giphy.com/v1/gifs/translate?api_key=Ov6f2QLfP2N2MdUGNrNlJ8vaPw2l9wWA&s=anime",
-	{ mode: "cors" }
-)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (response) {
-		img.src = response.data.images.original.url;
-	});
+function loadGif(paraQuery, query = "cat") {
+	query = paraQuery ? paraQuery : query;
+
+	fetch(
+		`https://api.giphy.com/v1/gifs/translate?api_key=Ov6f2QLfP2N2MdUGNrNlJ8vaPw2l9wWA&s=${query}`,
+		{ mode: "cors" }
+	)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (response) {
+			img.src = response.data.images.original.url;
+			errorDisplay.innerHTML = null;
+		})
+		.catch(function () {
+			errorDisplay.innerHTML = "GIF Guru can't find what you want right now :(";
+		});
+}
+
+window.onload = loadGif();
+
+nextGifBtn.addEventListener("click", () => {
+	loadGif(searchBox.value);
+});
+
+searchBtn.addEventListener("click", () => {
+	loadGif(searchBox.value);
+});
+
+searchBox.addEventListener("keyup", (event) => {
+	if (event.key !== "Enter") return;
+	searchBtn.click();
+	event.preventDefault;
+});
