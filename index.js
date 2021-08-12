593fc54085ceb7506dc07d8620350b173eb9dede
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-12 12:52:23 +0700

    Refactor to using async/await

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const searchBox = document.getElementById("search");
 const searchBtn = document.getElementById("search-button");
 const errorDisplay = document.getElementById("error");
 
-function loadGif(paraQuery, query = "aesthetic") {
+async function loadGif(paraQuery, query = "aesthetic") {
 	query = paraQuery ? paraQuery : query;
-
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=Ov6f2QLfP2N2MdUGNrNlJ8vaPw2l9wWA&s=${query}`,
-		{ mode: "cors" }
-	)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			img.src = response.data.images.original.url;
-			errorDisplay.innerHTML = null;
-		})
-		.catch(function () {
-			errorDisplay.innerHTML = "GIF Guru can't find what you want right now :(";
-		});
+	try {
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=Ov6f2QLfP2N2MdUGNrNlJ8vaPw2l9wWA&s=${query}`,
+			{ mode: "cors" }
+		);
+		const responseData = await response.json();
+		img.src = responseData.data.images.original.url;
+		errorDisplay.innerHTML = null;
+	} catch {
+		errorDisplay.innerHTML = "GIF Guru can't find what you want right now :(";
+	}
 }
 
 window.onload = loadGif();
